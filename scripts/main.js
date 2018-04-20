c4070c34465168bfaa29f8cd3e2b714bb0cdb8bf
quan.zhou 周权 <z31125@hytera.com>
2018-04-20 14:46:56 +0800

    Finish the project

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]'; //常量用大写
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = "hidden-detail";
+var TINY_EFFECT_CLASS = "is-tiny";
+var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
   'use strict';
@@ -23,9 +27,47 @@ function setDetailsFromThumb(thumbnail) {
   setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
-var firstThumbnail = document.querySelector(THUMBNAIL_LINK_SELECTOR);
-firstThumbnail.addEventListener("click", function(event) {
+function addThumbClickHandler(thumb) {
+  thumb.addEventListener("click", function (event){
     event.preventDefault();
-    console.log("You Clicked");
-    console.log(event);
-});
+    setDetailsFromThumb(thumb);
+    showDetails();
+  });
+}
+
+function getThumbnailsArray() {
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnailArray = [].slice.call(thumbnails);
+  return thumbnailArray;
+}
+
+function hideDetails() {
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function () {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
+}
+
+function addKeyPressHandler(){
+  document.body.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY) {
+      hideDetails();
+    }
+  });
+}
+
+function initializeEvents() {
+  var thumbnails = getThumbnailsArray();
+  thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
+}
+
+initializeEvents();
